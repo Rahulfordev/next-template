ff4268377a64194ad87688b5b0b66e6cf0b62494
rahulfordev <rahulforsms@gmail.com>
2024-07-11 13:07:18 +0600

    second

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,10 @@ import React from "react";
 
 const Header = ({ toggleSidebar }) => {
   return (
-    <header className="flex justify-between items-center p-6 bg-green-700 border-b border-gray-300 md:col-start-2 md:col-end-5">
-      <div className=" md:hidden" onClick={toggleSidebar}>
+    <header className="flex justify-between items-center p-6 bg-gray-100 border-b border-gray-300 md:col-start-2 md:col-end-5">
+      <div className="menu-icon cursor-pointer" onClick={toggleSidebar}>
         <span className="material-icons-outlined">menu</span>
       </div>
-      <h1>header</h1>
     </header>
   );
 };
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,67 +2,83 @@ import React from "react";
 
 const Main = () => {
   return (
-    <main className="p-5 overflow-y-auto md:col-start-2 md:col-end-5">
+    <main className="p-5 overflow-y-auto md:col-start-2 md:col-end-5 md:row-start-2">
       <div className="w-full h-full bg-red-500">
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
         <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
-          iure esse obcaecati ipsum rem corrupti delectus nihil possimus
-          exercitationem! Voluptatem eum sed asperiores odio fugiat repudiandae
-          ex, dignissimos unde debitis!
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>{" "}
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>{" "}
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>{" "}
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>{" "}
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>{" "}
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
+        </h1>{" "}
+        <h1>
+          Culpa pariatur sint non aute sit cillum labore occaecat qui voluptate
+          ex est eiusmod.
         </h1>
       </div>
     </main>
