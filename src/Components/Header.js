a3f3b58f702c50e3c3481bd1fac120094c5c5741
rahulfordev <rahulforsms@gmail.com>
2024-07-11 21:04:25 +0600

    sidebar content add

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,9 @@ const Header = ({ toggleSidebar }) => {
   return (
     <header className="flex justify-between items-center p-6 bg-gray-100 border-b border-gray-300 md:col-start-2 md:col-end-5">
       <div className="menu-icon cursor-pointer" onClick={toggleSidebar}>
-        <span className="material-icons-outlined">menu</span>
+        <span className="md:hidden">open sidebar</span>
       </div>
+      <h1>header</h1>
     </header>
   );
 };
